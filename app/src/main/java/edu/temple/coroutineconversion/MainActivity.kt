a955116231e4a5c170731000677232ef94ed2195
Alex09190 <tuj09190@temple.edu>
2023-11-02 14:44:38 -0400

    added coroutine

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,19 +23,36 @@ class MainActivity : AppCompatActivity() {
         true
     })
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         cakeImageView = findViewById(R.id.imageView)
 
-        findViewById<Button>(R.id.revealButton).setOnClickListener{
+        val scope = CoroutineScope(Job() + Dispatchers.Default)
+
+
+
+
+        findViewById<Button>(R.id.revealButton).setOnClickListener {
+
+            scope.launch {
+                repeat(100) {
+                    handler.sendEmptyMessage(it)
+                    delay(40)
+                }
+
+                /*
             Thread{
                 repeat(100) {
                     handler.sendEmptyMessage(it)
                     Thread.sleep(40)
                 }
             }.start()
+
+ */
+            }
         }
     }
 }
